66ba0b83699f66d473067c772bc1e9d93f93a37e
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-12 20:36:46 +0000

    build with relative paths not absolute

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    // This tells RsBuild to use relative paths
+    assetPrefix: './',
+  },
 });
