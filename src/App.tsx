6b65e6e7c95cf04feee71ae74742545ca5dd0e2f
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-12 19:09:05 +0000

    Remove the default components from the page

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,8 @@ function App(): JSX.Element {
   return (
     <>
       <Header />
-      <div className="content">
-        <h1>Rsbuild with React</h1>
-        <p>Start building amazing things with Rsbuild.</p>
-        <ItemDetailsTable />
-        <LatestPricesTable />
-      </div>
+      <ItemDetailsTable />
+      <LatestPricesTable />
     </>
   );
 }
