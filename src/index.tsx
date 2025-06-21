947ab2d4394126f6d7166e79810f3d78ab0a6e10
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-21 10:29:24 +0000

    set staletime to one minute

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootEl = document.getElementById('root');
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000,
+    },
+  },
+});
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
