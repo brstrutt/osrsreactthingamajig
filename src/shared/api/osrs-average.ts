500cc9eed54d770b9e6b531dcb6c9d0ed2aa03e6
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-21 19:36:26 +0000

    support the timestamp query param in the 5m and 1h apis

--- src/shared/api/osrs-average.ts
+++ src/shared/api/osrs-average.ts
@@ -4,6 +4,7 @@ import {
   UseSuspenseQueryResult,
 } from '@tanstack/react-query';
 import { useOsrsApiQueryOptions } from './osrs';
+import { useMemo } from 'react';
 
 export type AverageItemPriceData = {
   avgHighPrice: number | null;
@@ -18,24 +19,53 @@ export type AverageItemPrices = {
 
 type Response = { data: AverageItemPrices };
 
-export function useOsrs5mApi(): UseSuspenseQueryResult<Response, unknown> {
-  return useSuspenseQuery(useOsrs5mApiQueryOptions());
+export function useOsrs5mApi(
+  periodStartTime?: Date,
+): UseSuspenseQueryResult<Response, unknown> {
+  return useSuspenseQuery(useOsrs5mApiQueryOptions(periodStartTime));
 }
 
-export function useOsrs5mApiQueryOptions(): UseSuspenseQueryOptions<
-  Response,
-  unknown
-> {
-  return useOsrsApiQueryOptions('5m');
+export function useOsrs5mApiQueryOptions(
+  periodStartTime?: Date,
+): UseSuspenseQueryOptions<Response, unknown> {
+  const timestampUnixSeconds = useMemo(() => {
+    if (periodStartTime === undefined) return undefined;
+    const unixSeconds = periodStartTime.getTime() / 1000;
+    const roundedToFiveMinutes = Math.round(unixSeconds / 300) * 300;
+    return roundedToFiveMinutes;
+  }, [periodStartTime]);
+
+  const queryParams = useMemo(
+    () =>
+      timestampUnixSeconds === undefined
+        ? undefined
+        : new URLSearchParams({ timestamp: timestampUnixSeconds.toString() }),
+    [timestampUnixSeconds],
+  );
+  return useOsrsApiQueryOptions('5m', queryParams);
 }
 
-export function useOsrs1hApi(): UseSuspenseQueryResult<Response, unknown> {
-  return useSuspenseQuery(useOsrs1hApiQueryOptions());
+export function useOsrs1hApi(
+  periodStartTime?: Date,
+): UseSuspenseQueryResult<Response, unknown> {
+  return useSuspenseQuery(useOsrs1hApiQueryOptions(periodStartTime));
 }
 
-export function useOsrs1hApiQueryOptions(): UseSuspenseQueryOptions<
-  Response,
-  unknown
-> {
-  return useOsrsApiQueryOptions('1h');
+export function useOsrs1hApiQueryOptions(
+  periodStartTime?: Date,
+): UseSuspenseQueryOptions<Response, unknown> {
+  const timestampUnixSeconds = useMemo(() => {
+    if (periodStartTime === undefined) return undefined;
+    periodStartTime.setMinutes(0, 0, 0); // Drop anything smaller than hours. API rejects timestamps that aren't on the hour
+    return periodStartTime.getTime() / 1000;
+  }, [periodStartTime]);
+
+  const queryParams = useMemo(
+    () =>
+      timestampUnixSeconds === undefined
+        ? undefined
+        : new URLSearchParams({ timestamp: timestampUnixSeconds.toString() }),
+    [timestampUnixSeconds],
+  );
+  return useOsrsApiQueryOptions('1h', queryParams);
 }
--- src/shared/api/osrs.ts
+++ src/shared/api/osrs.ts
@@ -5,18 +5,25 @@ export type OsrsEndpoints = 'latest' | 'mapping' | '5m' | '1h' | 'timeseries';
 
 export function useOsrsApiQueryOptions<Response>(
   endpoint: OsrsEndpoints,
+  queryParams?: URLSearchParams,
 ): UseSuspenseQueryOptions<Response, unknown> {
-  return useMemo(() => getOsrsApiQueryOptions(endpoint), [endpoint]);
+  return useMemo(
+    () => getOsrsApiQueryOptions(endpoint, queryParams),
+    [endpoint, queryParams],
+  );
 }
 
 export function getOsrsApiQueryOptions<Response>(
   endpoint: OsrsEndpoints,
+  queryParams?: URLSearchParams,
 ): UseSuspenseQueryOptions<Response, unknown> {
+  const queryParamsString = queryParams?.toString();
+  let url = 'https://prices.runescape.wiki/api/v1/osrs/' + endpoint;
+  if (queryParamsString) {
+    url += '?' + queryParamsString;
+  }
   return {
-    queryKey: ['osrs', endpoint],
-    queryFn: async () =>
-      (
-        await fetch('https://prices.runescape.wiki/api/v1/osrs/' + endpoint)
-      ).json(),
+    queryKey: ['osrs', endpoint, queryParamsString],
+    queryFn: async () => (await fetch(url)).json(),
   };
 }
