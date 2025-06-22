1f494dce4a4991592f8ee35fe61a1c4957df717b
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-22 14:32:31 +0000

    make the tab buttons look more like tabs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import HighAlchProfitTable from './tables/high-alch-profit-table';
 import PickupProfitTable from './tables/pickup-profit-table';
 
-type Tabs = 'highalch' | 'pickup';
+type Tabs = 'highalch' | 'pickup' | 'other';
 
 function App(): JSX.Element {
   const [currentTab, setCurrentTab] = useState<Tabs>('highalch');
@@ -11,25 +11,35 @@ function App(): JSX.Element {
   return (
     <>
       <header>
-        React OSRS Price Thingamajig
-        <div>
+        <div className="title">React OSRS Price Thingamajig</div>
+        <div className="tabBar">
           <button
             onClick={() => setCurrentTab('highalch')}
             disabled={currentTab === 'highalch'}
+            className="tab"
           >
             High Alch
           </button>
           <button
             onClick={() => setCurrentTab('pickup')}
             disabled={currentTab === 'pickup'}
+            className="tab"
           >
             Pickup
+          </button>
+          <button
+            onClick={() => setCurrentTab('other')}
+            disabled={currentTab === 'other'}
+            className="tab"
+          >
+            Other
           </button>
         </div>
       </header>
       <main>
         {currentTab === 'highalch' && <HighAlchProfitTable />}
         {currentTab === 'pickup' && <PickupProfitTable />}
+        {currentTab === 'other' && <div>Other</div>}
       </main>
     </>
   );
