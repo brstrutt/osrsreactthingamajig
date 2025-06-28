a029286a7aaf3f6dd5bf34f05369689aaa660309
brstrutt <ben.strutt@hotmail.co.uk>
2025-06-28 10:44:49 +0000

    Update item lists to use names instead of Ids

--- src/tables/pickup-profit-table-items.ts
+++ src/tables/pickup-profit-table-items.ts
@@ -1,72 +1,74 @@
-const knownGoodPickupItemIds = [
-  273, //Poison(item)
-  272, //Fish food
-  544, //Monk robe top
-  542, //Monk robe bottoms
+import { ItemId } from '../shared';
+
+const knownGoodPickupItems = [
+  ItemId['Poison (item)'],
+  ItemId['Fish food'],
+  ItemId["Monk's robe top"],
+  ItemId["Monk's robe"],
 ];
 
 const allF2pNonWildernessItemPickups = [
-  1061, // Leather boots
-  1059, // Leather gloves
-  1511, // Logs
-  946, // Knife
-  1935, // Jug
-  1923, // Bowl
-  1931, // Pot
-  1925, // Bucket
-  1965, // Cabbage
-  1957, // Onion
-  1942, // Potatoes
-  1944, // Egg
-  314, // Feather
-  882, // Bronze arrow
-  1205, // Bronze dagger
-  556, // Air rune
-  555, // Water rune
-  557, // Earth rune
-  554, // Fire rune
-  558, // Mind rune
-  559, // Body rune
-  2313, // pie dish
-  1734, // Thread
-  952, // Spade
-  1009, // Brass necklace
-  2357, // Gold bar
-  223, // Red spiders eggs
-  526, // Bones
-  1982, // Tomato
-  1985, // Cheese
-  877, // Bronze bolts
-  1005, // White apron
-  1963, // Banana
-  1735, // Shears
-  245, // Wine of Zamorak
+  ItemId['Leather boots'],
+  ItemId['Leather gloves'],
+  ItemId['Logs'],
+  ItemId['Knife'],
+  ItemId['Jug'],
+  ItemId['Bowl'],
+  ItemId['Pot'],
+  ItemId['Bucket'],
+  ItemId['Cabbage'],
+  ItemId['Onion'],
+  ItemId['Potato'],
+  ItemId['Egg'],
+  ItemId['Feather'],
+  ItemId['Bronze arrow#(unp)'],
+  ItemId['Bronze dagger#(unp)'],
+  ItemId['Air rune'],
+  ItemId['Water rune'],
+  ItemId['Earth rune'],
+  ItemId['Fire rune'],
+  ItemId['Mind rune'],
+  ItemId['Body rune'],
+  ItemId['Pie dish'],
+  ItemId['Thread'],
+  ItemId['Spade'],
+  ItemId['Brass necklace'],
+  ItemId['Gold bar'],
+  ItemId["Red spiders' eggs"],
+  ItemId['Bones'],
+  ItemId['Tomato'],
+  ItemId['Cheese'],
+  ItemId['Bronze bolts#Unpoisoned'],
+  ItemId['White apron'],
+  ItemId['Banana'],
+  ItemId['Shears'],
+  ItemId['Wine of zamorak'],
 ];
 
 export const allF2pWildernessItemPickups = [
-  562, // Chaos rune
-  2351, // Iron bar
-  1887, // Cake tin
-  960, // Plank
-  837, // Crossbow
-  1203, // Iron dagger
-  1137, // Iron med helm
-  1207, // Steel dagger
-  1281, // Steel sword
-  1191, // Iron kiteshield
-  444, // Gold ore
-  1069, // Steel platelegs
-  1323, // Iron scimitar
-  561, // Nature rune
-  1119, // Steel platebody
-  1385, // Staff of Earth
-  1654, // Gold necklace
-  1607, // Sapphire
-  564, // Cosmic rune
+  ItemId['Chaos rune'],
+  ItemId['Iron bar'],
+  ItemId['Cake tin'],
+  ItemId['Plank'],
+  ItemId['Crossbow'],
+  ItemId['Iron dagger#(unp)'],
+  ItemId['Iron med helm'],
+  ItemId['Steel dagger#(unp)'],
+  ItemId['Steel sword'],
+  ItemId['Iron kiteshield'],
+  ItemId['Gold ore'],
+  ItemId['Steel platelegs'],
+  ItemId['Iron scimitar'],
+  ItemId['Nature rune'],
+  ItemId['Steel platebody'],
+  ItemId['Staff of earth'],
+  ItemId['Gold necklace'],
+  ItemId['Sapphire'],
+  ItemId['Cosmic rune'],
 ];
 
 export const allF2pPickupItems = [
-  ...knownGoodPickupItemIds,
+  ...knownGoodPickupItems,
   ...allF2pNonWildernessItemPickups,
   ...allF2pWildernessItemPickups,
 ];
--- src/tables/shop-flipping-profit-table-items.ts
+++ src/tables/shop-flipping-profit-table-items.ts
@@ -1,313 +1,306 @@
+import { ItemId } from '../shared';
+
 const generalStoreItems = [
-  1931, // Pot
-  1935, // Jug
-  1735, // Shears
-  1925, // Bucket
-  1923, // Bowl
-  1887, // Cake tin
-  590, // tinderbox
-  1755, // chisel
-  2347, // hammer
-  9003, // Security book
+  ItemId['Pot'],
+  ItemId['Jug'],
+  ItemId['Shears'],
+  ItemId['Bucket'],
+  ItemId['Bowl'],
+  ItemId['Cake tin'],
+  ItemId['Tinderbox'],
+  ItemId['Chisel'],
+  ItemId['Hammer'],
+  ItemId['Security book'],
 ];
 
 const lumbridgeGeneralStoreItems = [
   ...generalStoreItems,
-  946, // Knife
-  952, // Spade
+  ItemId['Knife'],
+  ItemId['Spade'],
 ];
 
 const archeryStoreItems = [
-  882, // Bronze arrow
-  884, // Iron Arrow
-  886, // Steel Arrow
-  888, // Mithril Arrow
-  890, // Adamant Arrow
-  877, // Bronze bolts
-  841, // Shortbow
-  839, // Longbow
-  843, // Oak Shortbow
-  845, // Oak Longbow
-  849, // Willow Shortbow
-  847, // Willow Longbow
-  853, // Maple Shortbow
-  851, // Maple Longbow
-  837, // Crossbow
+  ItemId['Bronze arrow#(unp)'],
+  ItemId['Iron arrow#(unp)'],
+  ItemId['Steel arrow#(unp)'],
+  ItemId['Mithril arrow#(unp)'],
+  ItemId['Adamant arrow#(unp)'],
+  ItemId['Bronze bolts#Unpoisoned'],
+  ItemId['Shortbow'],
+  ItemId['Longbow'],
+  ItemId['Oak shortbow'],
+  ItemId['Oak longbow'],
+  ItemId['Willow shortbow'],
+  ItemId['Willow longbow'],
+  ItemId['Maple shortbow'],
+  ItemId['Maple longbow'],
+  ItemId['Crossbow'],
 ];
 
 const axeStoreItems = [
-  1265, // Bronze pickaxe
-  1351, // Bronze Axe
-  1349, // Iron Axe
-  1353, // Steel Axe
-  1375, // Bronze Battleaxe
-  1363, // Iron Battleaxe
-  1365, // Steel Battleaxe
-  1367, // Black Battleaxe
-  1369, // Mithril Battleaxe
-  1371, // Adamant Battleaxe
+  ItemId['Bronze pickaxe'],
+  ItemId['Bronze axe'],
+  ItemId['Iron axe'],
+  ItemId['Steel axe'],
+  ItemId['Bronze battleaxe'],
+  ItemId['Iron battleaxe'],
+  ItemId['Steel battleaxe'],
+  ItemId['Black battleaxe'],
+  ItemId['Mithril battleaxe'],
+  ItemId['Adamant battleaxe'],
 ];
 
 const maceStoreItems = [
-  1422, // Bronze Mace
-  1420, // Iron Mace
-  1424, // Steel Mace
-  1428, // Mithril Mace
-  1430, // Adamant Mace
+  ItemId['Bronze mace'],
+  ItemId['Iron mace'],
+  ItemId['Steel mace'],
+  ItemId['Mithril mace'],
+  ItemId['Adamant mace'],
 ];
 
 const scimitarStoreItems = [
-  1321, // Bronze Scimitar
-  1323, // Iron Scimitar
-  1325, // Steel Scimitar
-  1329, // Mithril Scimitar
+  ItemId['Bronze scimitar'],
+  ItemId['Iron scimitar'],
+  ItemId['Steel scimitar'],
+  ItemId['Mithril scimitar'],
 ];
 
 const swordStoreItems = [
-  1277, // Bronze Sword
-  1279, // Iron Sword
-  1281, // Steel Sword
-  1283, // Black Sword
-  1285, // Mithril Sword
-  1287, // Adamant Sword
-  1291, // Bronze Longsword
-  1293, // Iron Longsword
-  1295, // Steel Longsword
-  1297, // Black Longsword
-  1299, // Mithril Longsword
-  1301, // Adamant Longsword
-  1205, // Bronze Dagger
-  1203, // Iron dagger
-  1207, // Steel Dagger
-  1217, // Black Dagger
-  1209, // Mithril Dagger
-  1211, // Adamant Dagger
+  ItemId['Bronze sword'],
+  ItemId['Iron sword'],
+  ItemId['Steel sword'],
+  ItemId['Black sword'],
+  ItemId['Mithril sword'],
+  ItemId['Adamant sword'],
+  ItemId['Bronze longsword'],
+  ItemId['Iron longsword'],
+  ItemId['Steel longsword'],
+  ItemId['Black longsword'],
+  ItemId['Mithril longsword'],
+  ItemId['Adamant longsword'],
+  ItemId['Bronze dagger#(unp)'],
+  ItemId['Iron dagger#(unp)'],
+  ItemId['Steel dagger#(unp)'],
+  ItemId['Black dagger#(unp)'],
+  ItemId['Mithril dagger#(unp)'],
+  ItemId['Adamant dagger#(unp)'],
 ];
 
 const staffStoreItems = [
-  1379, // Staff
-  1389, // Magic Staff
-  1381, // Staff of Air
-  1383, // Staff of Water
-  1385, // Staff of Earth
-  1387, // Staff of Fire
+  ItemId['Staff'],
+  ItemId['Magic staff'],
+  ItemId['Staff of air'],
+  ItemId['Staff of water'],
+  ItemId['Staff of earth'],
+  ItemId['Staff of fire'],
 ];
 
 const pubItems = [
-  1917, // Beer
-  1905, // Asgarnian Ale
-  1907, // Wizard's Mind Bomb
-  1913, // Dwarven Stout
-  1993, // Jug of Wine
+  ItemId['Beer'],
+  ItemId['Asgarnian ale'],
+  ItemId["Wizard's mind bomb"],
+  ItemId['Dwarven stout'],
+  ItemId['Jug of wine'],
 ];
 
-const wineTraderItems = [
-  1993, // Jug of Wine
-  1935, // Jug
-];
+const wineTraderItems = [ItemId['Jug of wine'], ItemId['Jug']];
 
 const armourStoreItems = [
-  1103, // Bronze Chainbody
-  1101, // Iron Chainbody
-  1105, // Steel Chainbody
-  1107, // Black Chainbody
-  1109, // Mithril Chainbody
-  1111, // Adamant Chainbody
-  1117, // Bronze Platebody
-  1115, // Iron Platebody
-  1119, // Steel platebody
-  1125, // Black Platebody
-  1121, // Mithril Platebody
-  1075, // Bronze Platelegs
-  1067, // Iron Platelegs
-  1069, // Steel platelegs
-  1077, // Black Platelegs
-  1071, // Mithril Platelegs
-  1073, // Adamant Platelegs
-  1087, // Bronze Plateskirt
-  1081, // Iron Plateskirt
-  1083, // Steel Plateskirt
-  1089, // Black Plateskirt
-  1085, // Mithril Plateskirt
-  1091, // Adamant Plateskirt
-  1133, // Studded Body
-  1097, // Studded Chaps
+  ItemId['Bronze chainbody'],
+  ItemId['Iron chainbody'],
+  ItemId['Steel chainbody'],
+  ItemId['Black chainbody'],
+  ItemId['Mithril chainbody'],
+  ItemId['Adamant chainbody'],
+  ItemId['Bronze platebody'],
+  ItemId['Iron platebody'],
+  ItemId['Steel platebody'],
+  ItemId['Black platebody'],
+  ItemId['Mithril platebody'],
+  ItemId['Bronze platelegs'],
+  ItemId['Iron platelegs'],
+  ItemId['Steel platelegs'],
+  ItemId['Black platelegs'],
+  ItemId['Mithril platelegs'],
+  ItemId['Adamant platelegs'],
+  ItemId['Bronze plateskirt'],
+  ItemId['Iron plateskirt'],
+  ItemId['Steel plateskirt'],
+  ItemId['Black plateskirt'],
+  ItemId['Mithril plateskirt'],
+  ItemId['Adamant plateskirt'],
+  ItemId['Studded body'],
+  ItemId['Studded chaps'],
 ];
 
 const helmetStoreItems = [
-  1139, // Bronze Med Helm
-  1137, // Iron med helm
-  1141, // Steel Med Helm
-  1143, // Mithril Med Helm
-  1145, // Adamant Med Helm
-  1155, // Bronze Full Helm
-  1153, // Iron Full Helm
-  1157, // Steel Full Helm
-  1159, // Mithril Full Helm
-  1161, // Adamant Full Helm
+  ItemId['Bronze med helm'],
+  ItemId['Iron med helm'],
+  ItemId['Steel med helm'],
+  ItemId['Mithril med helm'],
+  ItemId['Adamant med helm'],
+  ItemId['Bronze full helm'],
+  ItemId['Iron full helm'],
+  ItemId['Steel full helm'],
+  ItemId['Mithril full helm'],
+  ItemId['Adamant full helm'],
 ];
 
 const shieldStoreItems = [
-  1171, // Wooden Shield
-  1173, // Bronze Square Shield
-  1189, // Bronze Kiteshield
-  1175, // Iron Square Shield
-  1191, // Iron kiteshield
-  1177, // Steel Square Shield
-  1193, // Steel Kiteshield
-  1181, // Mithril Square Shield
+  ItemId['Wooden shield'],
+  ItemId['Bronze sq shield'],
+  ItemId['Bronze kiteshield'],
+  ItemId['Iron sq shield'],
+  ItemId['Iron kiteshield'],
+  ItemId['Steel sq shield'],
+  ItemId['Steel kiteshield'],
+  ItemId['Mithril sq shield'],
 ];
 
-const silkTrader = [
-  9510, // Silk
-];
+const silkTrader = [ItemId['Silk']];
 
 const shopOfChampionsItems = [
-  1021, // Blue Cape
-  1165, // Black Full Helm
-  1077, // Black Platelegs
-  1123, // Adamant Platebody
+  ItemId['Blue cape'],
+  ItemId['Black full helm'],
+  ItemId['Black platelegs'],
+  ItemId['Adamant platebody'],
 ];
 
 const runeStoreItems = [
-  1093, // Rune Plateskirt
-  1079, // Rune Platelegs
-  1432, // Rune Mace
-  1113, // Rune Chainbody
-  1303, // Rune Longsword
-  1289, // Rune Sword
-  1099, // Green D'hide Chaps
-  1065, // Green D'hide Vambraces
-  1169, // Coif
+  ItemId['Rune plateskirt'],
+  ItemId['Rune platelegs'],
+  ItemId['Rune mace'],
+  ItemId['Rune chainbody'],
+  ItemId['Rune longsword'],
+  ItemId['Rune sword'],
+  ItemId["Green d'hide chaps"],
+  ItemId["Green d'hide vambraces"],
+  ItemId['Coif'],
 ];
 
 const clothingStoreItems = [
-  1005, // White apron
-  1129, // Leather Body
-  1059, // Leather Gloves
-  1061, // Leather Boots
-  1013, // Pink Skirt
-  1015, // Black Skirt
-  1011, // Blue Skirt
-  1007, // Red Cape
-  9510, // Silk
-  428, // Priest Gown (bottom)
-  426, // Priest Gown (top)
+  ItemId['White apron'],
+  ItemId['Leather body'],
+  ItemId['Leather gloves'],
+  ItemId['Leather boots'],
+  ItemId['Pink skirt'],
+  ItemId['Black skirt'],
+  ItemId['Blue skirt'],
+  ItemId['Red cape'],
+  ItemId['Silk'],
+  ItemId['Priest gown (bottom)'],
+  ItemId['Priest gown (top)'],
 ];
 
 const craftingStoreItems = [
-  1755, // Chisel
-  1592, // Ring Mould
-  1597, // Necklace Mould
-  1595, // Amulet Mould
-  1733, // Needle
-  1734, // Thread
-  1599, // Holy Mould
-  5523, // Tiara Mould
+  ItemId['Chisel'],
+  ItemId['Ring mould'],
+  ItemId['Necklace mould'],
+  ItemId['Amulet mould'],
+  ItemId['Needle'],
+  ItemId['Thread'],
+  ItemId['Holy mould'],
+  ItemId['Tiara mould'],
 ];
 
 const fishingStoreItems = [
-  303, // Small Fishing Net
-  307, // Fishing Rod
-  309, // Fly Fishing Rod
-  311, // Harpoon
-  301, // Lobster Pot
-  313, // Fishing Bait
-  314, // Feather
-  317, // Raw Shrimps
-  327, // Raw Sardine
-  345, // Raw Herring
-  321, // Raw Anchovies
-  335, // Raw Trout
-  349, // Raw Pike
-  331, // Raw Salmon
-  359, // Raw Tuna
-  377, // Raw Lobster
-  371, // Raw Swordfish
+  ItemId['Small fishing net'],
+  ItemId['Fishing rod'],
+  ItemId['Fly fishing rod'],
+  ItemId['Harpoon'],
+  ItemId['Lobster pot'],
+  ItemId['Fishing bait'],
+  ItemId['Feather'],
+  ItemId['Raw shrimps'],
+  ItemId['Raw sardine'],
+  ItemId['Raw herring'],
+  ItemId['Raw anchovies'],
+  ItemId['Raw trout'],
+  ItemId['Raw pike'],
+  ItemId['Raw salmon'],
+  ItemId['Raw tuna'],
+  ItemId['Raw lobster'],
+  ItemId['Raw swordfish'],
 ];
 
 const foodStoreItems = [
-  1933, // Pot of Flour
-  2132, // Raw Beef
-  2138, // Raw Chicken
-  1965, // Cabbage
-  1963, // Banana
-  1951, // Redberries
-  2309, // Bread
-  1973, // Chocolate Bar
-  1985, // Cheese
-  1982, // Tomato
-  1942, // Potato
-  1971, // Kebab
+  ItemId['Pot of flour'],
+  ItemId['Raw beef'],
+  ItemId['Raw chicken'],
+  ItemId['Cabbage'],
+  ItemId['Banana'],
+  ItemId['Redberries'],
+  ItemId['Bread'],
+  ItemId['Chocolate bar'],
+  ItemId['Cheese'],
+  ItemId['Tomato'],
+  ItemId['Potato'],
+  ItemId['Kebab'],
 ];
 
-const furTraderItems = [
-  948, // Bear Fur
-];
+const furTraderItems = [ItemId['Bear fur']];
 
 const gemTraderItems = [
-  1623, // Uncut Sapphire
-  1621, // Uncut Emerald
-  1619, // Uncut Ruby
-  1617, // Uncut Diamond
-  1607, // Sapphire
-  1605, // Emerald
-  1603, // Ruby
-  1601, // Diamond
+  ItemId['Uncut sapphire'],
+  ItemId['Uncut emerald'],
+  ItemId['Uncut ruby'],
+  ItemId['Uncut diamond'],
+  ItemId['Sapphire'],
+  ItemId['Emerald'],
+  ItemId['Ruby'],
+  ItemId['Diamond'],
 ];
 
 const grumsGoldExchange = [
-  1635, // Gold Ring
-  1637, // Sapphire Ring
-  1639, // Emerald Ring
-  1641, // Ruby Ring
-  1643, // Diamond Ring
-  1654, // Gold necklace
-  1656, // Sapphire Necklace
-  1658, // Emerald Necklace
-  1660, // Ruby Necklace
-  1662, // Diamond Necklace
-  1692, // Gold Amulet
-  1694, // Sapphire Amulet
-  1696, // Emerald Amulet
-  1698, // Ruby Amulet
-  1700, // Diamond Amulet
+  ItemId['Gold ring'],
+  ItemId['Sapphire ring'],
+  ItemId['Emerald ring'],
+  ItemId['Ruby ring'],
+  ItemId['Diamond ring'],
+  ItemId['Gold necklace'],
+  ItemId['Sapphire necklace'],
+  ItemId['Emerald necklace'],
+  ItemId['Ruby necklace'],
+  ItemId['Diamond necklace'],
+  ItemId['Gold amulet'],
+  ItemId['Sapphire amulet'],
+  ItemId['Emerald amulet'],
+  ItemId['Ruby amulet'],
+  ItemId['Diamond amulet'],
 ];
 
 const magicShopItems = [
-  554, // Fire rune
-  555, // Water rune
-  556, // Air rune
-  557, // Earth rune
-  558, // Mind rune
-  559, // Body rune
-  562, // Chaos rune
-  560, // Death Rune
-  221, // Eye of Newt
-  579, // Blue Wizard Hat
-  1017, // Wizard Hat
+  ItemId['Fire rune'],
+  ItemId['Water rune'],
+  ItemId['Air rune'],
+  ItemId['Earth rune'],
+  ItemId['Mind rune'],
+  ItemId['Body rune'],
+  ItemId['Chaos rune'],
+  ItemId['Death rune'],
+  ItemId['Eye of newt'],
+  ItemId['Blue wizard hat'],
+  ItemId['Wizard hat'],
 ];
 
 const miningShopItems = [
-  2347, // Hammer
-  1265, // Bronze Pickaxe
-  1267, // Iron Pickaxe
-  1269, // Steel Pickaxe
-  1273, // Mithril Pickaxe
-  1271, // Adamant Pickaxe
-  1275, // Rune Pickaxe
-  436, // Copper Ore
-  438, // Tin Ore
-  440, // Iron Ore
-  453, // Coal
-  2349, // Bronze Bar
-  2351, // Iron bar
-  2357, // Gold bar
+  ItemId['Hammer'],
+  ItemId['Bronze pickaxe'],
+  ItemId['Iron pickaxe'],
+  ItemId['Steel pickaxe'],
+  ItemId['Mithril pickaxe'],
+  ItemId['Adamant pickaxe'],
+  ItemId['Rune pickaxe'],
+  ItemId['Copper ore'],
+  ItemId['Tin ore'],
+  ItemId['Iron ore'],
+  ItemId['Coal'],
+  ItemId['Bronze bar'],
+  ItemId['Iron bar'],
+  ItemId['Gold bar'],
 ];
 
-const wildernessShopItems = [
-  2283, // Pizza Base
-];
+const wildernessShopItems = [ItemId['Pizza base']];
 
 export const allShopItems = [
   ...lumbridgeGeneralStoreItems,
