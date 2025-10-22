be021f8e7af42eb493132f41eeb0bce70fc950c6
brstrutt <ben.strutt@hotmail.co.uk>
2025-10-22 21:08:18 +0000

    install the react compiler

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+import { pluginBabel } from '@rsbuild/plugin-babel';
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginTypeCheck()],
+  plugins: [
+    pluginReact(),
+    pluginTypeCheck(),
+    pluginBabel({
+        include: /\.(?:jsx|tsx)$/,
+        babelLoaderOptions(opts) {
+          opts.plugins?.unshift('babel-plugin-react-compiler');
+        },
+      }),
+    ],
   output: {
     // This tells RsBuild to use relative paths
     assetPrefix: './',
